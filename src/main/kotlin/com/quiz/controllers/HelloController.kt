e7ca3f6f1265452a43f8d4a00a06fc1f0186f13e
Alejandra Vanesa Juarez D'Aquino <ale@emma.home>
2021-09-27 10:51:02 +0200

    http client configured

--- src/main/kotlin/com/quiz/controllers/HelloController.kt
+++ src/main/kotlin/com/quiz/controllers/HelloController.kt
@@ -3,12 +3,14 @@ package com.quiz.controllers
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
 
 @Controller("/hello")
-class HelloController {
+class HelloController( @Client("https://swapi.dev/api/") val httpClient: HttpClient ) {
     @Get(produces = [MediaType.TEXT_PLAIN])
     fun index(): String {
-        return "Hello World"
+        val result: String = httpClient.toBlocking().retrieve("/people/1/")
+        return "$result"
     }
 }
